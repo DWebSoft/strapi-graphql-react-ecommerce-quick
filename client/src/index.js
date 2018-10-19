77a68b28f138cece2f78365579dadb362f4d6dd7
DWebSoft <dwebsoftguy@gmail.com>
2018-10-19 13:35:52 +0530

    Add private route

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import "gestalt/dist/gestalt.css";
 
 import Navbar from "./components/Navbar";
@@ -10,6 +10,17 @@ import Signup from './components/Signup';
 import Checkout from './components/Checkout';
 import registerServiceWorker from './registerServiceWorker';
 import Brews from "./components/Brews";
+import { getToken } from "./utils";
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route {...rest} render={ props => (
+        getToken() !== null ? 
+        <Component {...props}/> : <Redirect to={{
+            pathname: '/signin',
+            state: { from: props.location }
+        }}/>
+    )}/>  
+);
 
 const Root = () => (
     <Router>
@@ -19,7 +30,7 @@ const Root = () => (
                 <Route component={App} exact path="/" />
                 <Route component={Signin} path="/signin" />
                 <Route component={Signup} path="/signup" />
-                <Route component={Checkout} path="/checkout" />
+                <PrivateRoute component={Checkout} path="/checkout" />
                 <Route component={Brews} path="/:brandId" />
             </Switch>
         </React.Fragment>    
